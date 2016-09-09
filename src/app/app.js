d370d91d93e29eb88f314940763c760a703b4303
Jack Lightfoot <jack.lightfoot@tee2.org>
2016-09-08 17:24:42 -0700

    favicon and title change

--- src/app/app.js
+++ src/app/app.js
@@ -12,7 +12,6 @@ import './reducers';
 require('file?name=manifest.json!json-file!json!../www/manifest.json');
 require('../www/index.html');
 require('../www/main.css');
-require('../www/favicon.ico');
 require('../images/icons/star_144.png');
 require('../images/icons/star_152.png');
 // Needed for onTouchTap
